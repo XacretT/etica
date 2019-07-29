72408850aae9991c9b1a102b6b1ee32f6b6fe02c
haema5 <killy.ny@gmail.com>
2019-07-29 16:47:17 +0300

    агдд

--- backend/db_peewee.py
+++ backend/db_peewee.py
@@ -17,8 +17,8 @@ class Base(Model):
 
 
 class Users(Base):
-    id = IntegerField(primary_key=True)
-    uid = TextField()
+    user_id = IntegerField(primary_key=True)
+    uid = TextField(unique=True)
     private_token = TextField()
     status = BooleanField(default='1')
 
@@ -27,8 +27,8 @@ class Users(Base):
 
 
 class Addresses(Base):
-    id = IntegerField(primary_key=True)
-    user_id = ForeignKeyField(Users, related_name='id')
+    address_id = IntegerField(primary_key=True)
+    user_id = ForeignKeyField(Users, to_field='user_id', related_name='users', unique=True)
     ip = TextField()
     port = TextField()
     timestamp = DateTimeField(default=datetime.utcnow())
@@ -42,37 +42,74 @@ def create_tables():
         database.create_tables([Users, Addresses])
 
 
-def create_user(data):
+def add_user(data=''):
     if data == '' : return
 
-    Users.create(
-        uid=data['uid'],
-        private_token=data['private_token']
-    )
+    try:
+        user = Users.create(
+            uid=data['uid'],
+            private_token=data['private_token']
+        )
+    except IntegrityError as e:
+        return print(str(e))
+    else:
+        return print('id:', user, data)
 
-    Addresses.create(
-        user_id=Users.select(Users.id).where(Users.uid == data['uid']),
-        # uid=data['uid'],
+
+def add_address(data=''):
+    if data == '' : return
+
+    address = Addresses.insert(
+        user_id=Users.select(Users.user_id).where(Users.uid == data['uid']).exists(),
         ip=data['ip'],
         port=data['port']
-    )
+    ).on_conflict('replace').execute()
 
-    return print(data)
+    return print('address_id:', address, data)
+
+
+def search_user(uid):
+    if uid == '' : return
+
+    
+
+
+def delete_user(data=''):
+    if data == '' : return
+
+    user_id = Users.select(Users.user_id).where(Users.uid == data['uid'])
+
+    erase_address = Addresses.delete().where(Addresses.user_id == user_id).execute()
+    erase_user = Users.delete().where(Users.uid == data['uid']).execute()
+
+    return print(erase_user, erase_address)
+
+
+def clear_addresses():
+    if data == '' : return
+
+    query = Addresses.select(Addresses.user_id)\
+        .join(Users, on=(Addresses.user_id == Users.user_id))\
+        .where(Users.user_id == None)
+
+    count = 0
+    for q in query:
+        Addresses.delete().where(Addresses.user_id == q).execute()
+        count += 1
+
+    return print(f'Removed {count} records.')
 
 
 def show_all():
-    Users.select()
-    Addresses.select()
+    users = Users.select()
+    address = Addresses.select()
 
 
 if __name__ == '__main__':
     create_tables()
-    private = 'private'
-    test = Addresses.create(ip='127.0.0.1', port='2323')
 
     data = {'uid': 'test', 'private_token': 'private', 'ip': '127.0.0.2', 'port': '8080'}
-    create_user(data)
-
-    show_all()
-
-    print("I'm sorry but not now.")
+    add_user(data)
+    add_address(data)
+    delete_user(data)
+    clear_addresses()
